e1d326c532fcb32fde24c5411459113713811737
Erik Oudsen <e.oudsen@gmail.com>
2022-12-06 19:16:52 +0100

    DAY-6: Add solutions for day 6 and prepare for day 7.

--- java/src/Day6/Day6.java
+++ java/src/Day6/Day6.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Day6 {
 
@@ -19,20 +22,29 @@ public class Day6 {
         return null;
     }
 
-    public static Integer part1(final List<Integer> input) {
-
-        return 0;
+    public static List<List<Character>> slidingWindow(final List<Character> inputList, final Integer windowSize) {
+        return IntStream.range(0, inputList.size() - windowSize + 1)
+                .mapToObj(start -> inputList.subList(start, start + windowSize))
+                .toList();
     }
 
-    public static Integer part2(final List<Integer> input) {
+    public static Integer part1(final List<String> input) {
+        List<Character> inputList = input.get(0).chars().mapToObj(var -> (char) var).toList();
+        List<List<Character>> windows = slidingWindow(inputList, 4);
+        List<Integer> setSizes = windows.stream().map(HashSet::new).map(Set::size).toList();
+        return setSizes.indexOf(4) + 4;
+    }
 
-        return 0;
+    public static Integer part2(final List<String> input) {
+        List<Character> inputList = input.get(0).chars().mapToObj(var -> (char) var).toList();
+        List<List<Character>> windows = slidingWindow(inputList, 14);
+        List<Integer> setSizes = windows.stream().map(HashSet::new).map(Set::size).toList();
+        return setSizes.indexOf(14) + 14;
     }
 
     public static void main(String... args) {
         var fileContent = getFileContent("java/src/day6/input.txt");
-        var intInput = fileContent.stream().map(Integer::parseInt).collect(Collectors.toList());
-        System.out.println(part1(intInput));
-        System.out.println(part2(intInput));
+        System.out.println(part1(fileContent)); // 1816
+        System.out.println(part2(fileContent)); // 2625
     }
 }
