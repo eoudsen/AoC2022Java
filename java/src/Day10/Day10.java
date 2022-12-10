d4aee701831d2d10a1f81b4445a7a9b87881075a
Erik Oudsen <e.oudsen@gmail.com>
2022-12-10 21:37:45 +0100

    DAY-10: Add solutions for day 10 and prepare for day 11.

--- java/src/Day10/Day10.java
+++ java/src/Day10/Day10.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,21 +20,82 @@ public class Day10 {
         return null;
     }
 
-    public static Integer part1(final List<Integer> input) {
-
-        return 0;
+    private static boolean checkSignalStrength(final int cycle) {
+        if ((cycle == 20 || (cycle - 20) % 40 == 0) && cycle < 221) {
+            return true;
+        }
+        return false;
     }
 
-    public static Integer part2(final List<Integer> input) {
+    public static Integer part1(final List<String> input) {
+        var signalStrengths = new ArrayList<Integer>();
+        var cycle = 0;
+        var xValue = 1;
+        for (var op : input) {
+            cycle += 1;
+            if (op.startsWith("noop")) {
+            }
+            if (op.startsWith("addx")) {
+                var valueToAdd = op.split(" ")[1];
+                var toSave = checkSignalStrength(cycle);
+                if (toSave) {
+                    signalStrengths.add(xValue * cycle);
+                }
+                cycle += 1;
+                toSave = checkSignalStrength(cycle);
+                if (toSave) {
+                    signalStrengths.add(xValue * cycle);
+                }
+                xValue += Integer.parseInt(valueToAdd);
+                continue;
+            }
+            var toSave = checkSignalStrength(cycle);
+            if (toSave) {
+                signalStrengths.add(xValue * cycle);
+            }
+        }
+        return signalStrengths.stream().reduce(0, Integer::sum);
+    }
 
+    public static void draw(int cycle, int xValue) {
+        cycle -= 1;
+        if (cycle % 40 == 0) {
+            System.out.println();
+        }
+        if (cycle >= 40) {
+            cycle = cycle % 40;
+        }
+        if (xValue - 1 <= cycle && cycle <= xValue + 1) {
+            System.out.print("#");
+        }
+        else {
+            System.out.print(".");
+        }
+    }
+
+    public static Integer part2(final List<String> input) {
+        int cycle = 0;
+        int xValue = 1;
+        for (var op : input) {
+            cycle += 1;
+            if (op.startsWith("noop")) {
+                draw(cycle, xValue);
+            }
+            if (op.startsWith("addx")) {
+                var valueToAdd = op.split(" ")[1];
+                draw(cycle, xValue);
+                cycle += 1;
+                draw(cycle, xValue);
+                xValue += Integer.parseInt(valueToAdd);
+            }
+        }
         return 0;
     }
 
     public static void main(String... args) {
         var fileContent = getFileContent("java/src/day10/input.txt");
-        var intInput = fileContent.stream().map(Integer::parseInt).collect(Collectors.toList());
-        System.out.println(part1(intInput));
-        System.out.println(part2(intInput));
+        System.out.println(part1(fileContent)); // 15360
+        System.out.println(part2(fileContent)); // PHLHJGZA
     }
 }
 
